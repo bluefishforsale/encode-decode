2a4fb7699f08274547cac25b637d239e682133cc
Terrac Skiens <terracskiens@Ryzentosh.localdomain>
2020-09-27 14:15:09 -0700

    update

---
+++ h.264_to_hevc.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+while getopts "dnf:" opt; do
+  case ${opt} in
+    d ) DELETE=TRUE ;;
+    n ) NVENC=TRUE ;;
+    f ) INFILE="${OPTARG}" ; echo $INFILE ;;
+    \? ) echo "Usage: cmd [-d] [-n] -f <filename>" ;;
+  esac
+done
+shift $((OPTIND-1))
+
+if [[ -z "${INFILE}" ]] ; then
+  echo "Missing argument: File to compress"
+  exit 1
+fi
+
+FILE="$(basename "${INFILE}")"
+DIRNAME="$(dirname "${INFILE}")"
+OUTFILE="${FILE%.*}.mp4"
+
+
+check_disk() {
+    FREE=$(df "$(pwd)"  | tail -n1 | awk '{print $4}')
+    [[ -n "$FREE" ]] || (echo "can't determine free space, stopping" ; exit 1)
+    [[ $FREE -ge 10000000 ]] || (echo "not enough free space, stopping" ; exit 1)
+}
+
+check_file() {
+    IN="${@%%----*}":while
+    OUT="${@##*----}"
+
+    IN_SIZE=$(du "${IN}" | awk '{print $1}')
+    IN_DUR=$(ffprobe -i "${IN}" 2>&1 | grep Duration | awk '{print $2}' | awk -F '.' '{print $1}')
+
+    OUT_SIZE=$(du "${OUT}" | awk '{print $1}')
+    OUT_DUR=$(ffprobe -i "${OUT}" 2>&1 | grep Duration | awk '{print $2}' | awk -F '.' '{print $1}')
+
+    echo $IN_SIZE $OUT_SIZE
+    echo $IN_DUR  $OUT_DUR
+
+    [[ -z "${OUT_DUR}" ]] && return 1
+    (( OUT_SIZE >= 100000000 )) || return 1
+    [[ "${IN_DUR}" == "${OUT_DUR}" ]] && return 0
+}
+
+pushd "${DIRNAME}"
+echo "Compressing ${FILE} now ..."
+
+[[ -n ${NVENC} ]] && IN_ENCODER="-hwaccel cuvid -c:v h264_cuvid" || IN_ENCODER=""
+[[ -n ${NVENC} ]] && OUT_ENCODER="-c:v hevc_nvenc" || OUT_ENCODER="-c:v libx265"
+[[ -f /usr/sbin/sysctl ]] && CORES=$(sysctl -n hw.ncpu) || CORES=$(grep processor /proc/cpuinfo | wc -l)
+# max 16 threads
+CORES=$(( ${CORES} > 16 ? 16 : ${CORES} ))
+
+
+
+#### action time
+check_disk
+set -x
+ffmpeg -y -vsync 0 ${IN_ENCODER} -i "${FILE}" -c:a copy -crf 26 -preset medium -threads ${CORES} ${OUT_ENCODER} "${OUTFILE}" && \
+    [[ -n "${DELETE}"  ]] && \
+        ( check_file "${FILE}----${OUTFILE}" && ( echo "removing ${FILE} now" ; rm -v "${FILE}" ))
+
+set +x
+popd
