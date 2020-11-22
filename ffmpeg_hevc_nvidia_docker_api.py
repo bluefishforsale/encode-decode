63b621498df34e46539ef7c3cec5de6f42da283e
terrac <terracnosaur@gmail.com>
2020-11-22 21:00:38 +0000

    remove the pretty errors

--- ffmpeg_hevc_nvidia_docker_api.py
+++ ffmpeg_hevc_nvidia_docker_api.py
@@ -12,22 +12,6 @@ import sys
 import time
 from pprint import pprint
 
-#import pretty_errors
-#pretty_errors.configure(
-#    separator_character = '*',
-#    filename_display    = pretty_errors.FILENAME_EXTENDED,
-#    line_number_first   = True,
-#    display_link        = True,
-#    lines_before        = 5,
-#    lines_after         = 2,
-#    line_length         = 80,
-#    line_color          = pretty_errors.RED + '> ' + pretty_errors.default_config.line_color,
-#    code_color          = '  ' + pretty_errors.default_config.line_color,
-#    truncate_code       = False,
-#    display_locals      = True
-#)
-#pretty_errors.replace_stderr()
-
 FF_IMAGE='linuxserver/ffmpeg'
 client = docker.from_env()
 api_client = docker.APIClient(base_url='unix://var/run/docker.sock')
