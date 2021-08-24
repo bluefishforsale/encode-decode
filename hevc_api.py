a15320b0b987bf6776d62567eeb876eeecdf8d5a
terrac <terracnosaur@gmail.com>
2021-08-24 02:57:52 +0000

    the docker nvec version

--- hevc_api.py
+++ hevc_api.py
@@ -12,24 +12,27 @@ import sys
 import time
 from pprint import pprint
 
-FF_IMAGE='linuxserver/ffmpeg'
+# FF_IMAGE='linuxserver/ffmpeg'
+FF_IMAGE='jrottenberg/ffmpeg'
+FF_TAG='4.1-nvidia'
+
 client = docker.from_env()
 api_client = docker.APIClient(base_url='unix://var/run/docker.sock')
 
 
 def remove(filename):
-  print(f"Removing file: {infile}")
+  print(f"Removing file: {filename}")
   subprocess.check_call(['sudo', 'rm', '-v', filename])
 
 
 def file_size_check(filename):
-  if os.path.exists(filename):
-    size = int(os.path.getsize(filename))
-    if size <= 4096:
-      print(f"Removing {size} byte file: {filename}")
-      remove(filename)
-      return False
+  if not os.path.exists(filename):
+    return False
+  size = int(os.path.getsize(filename))
+  if size >= 40960:
     return True
+  print(f"File: {filename} is {size} byte(s)" )
+  remove(filename)
   return False
 
 
@@ -50,22 +53,28 @@ def file_dur_check(infile, outfile):
       return True
 
 
-def get_container_name(task):
+def get_container_name(task, infile):
+  infile_clean = re.sub(r'[^a-zA-Z0-9]', '', infile)
+  if len(infile_clean) >= 11:
+      infile_trunc = infile_clean[:13]
+  else:
+      infile_trunc = infile_clean
+
   letters = string.ascii_uppercase + string.ascii_uppercase
-  suffix = ''.join(random.choice(letters) for i in range(16))
-  return f'{task}_{suffix}'
+  suffix = ''.join(random.choice(letters) for i in range(4))
+  return f'{infile_trunc}_{task}_{suffix}'
 
 
-def run_convert(infile, outfile, stream_mapi, in_codec_name):
+def run_convert(infile, outfile, stream_map, in_codec_name):
   (filedir, filename) = get_filepaths(infile)
   print(f"Converting: {infile} -> {outfile}")
   maps = []
   for stream_id in stream_map.keys():
     maps.extend(['-map', f'0:{stream_id}'])
-  name=get_container_name('ffmpeg'),
+  name=get_container_name('ffmpeg', infile),
   try:
     container = client.containers.run(
-      image=FF_IMAGE,
+      image=f'{FF_IMAGE}:{FF_TAG}',
       detach=True,
       runtime='nvidia',
       devices=['/dev/dri:/dev/dri'],
@@ -73,10 +82,12 @@ def run_convert(infile, outfile, stream_mapi, in_codec_name):
       volumes=[f'{filedir}:{filedir}'],
       name=name,
       entrypoint='/usr/local/bin/ffmpeg',
-      command=['-hide_banner', '-analyzeduration', '10M', '-probesize', '10M', '-vsync', '0',
-        '-fflags', '+igndts',
-        '-hwaccel', 'nvdec', '-c:v', in_codec_name, '-i', f'{filedir}/{infile}', '-max_muxing_queue_size', '2048',
-      ] + maps + [ '-crf', '10', '-c:a', 'copy', '-c:s', 'copy', '-c:v', 'hevc_nvenc', '-y', f'{filedir}/{outfile}' ]
+      command=[
+        '-hide_banner', '-analyzeduration', '10M', '-probesize', '10M', '-vsync', '0',
+        '-fflags', '+igndts', '-flags', '+global_header', '-hwaccel', 'nvdec', '-c:v', in_codec_name,
+        '-hwaccel_output_format', 'cuda', '-i', f'{filedir}/{infile}', '-max_muxing_queue_size', '2048',
+        '-crf', '1', '-c:a', 'copy', '-c:s', 'copy', '-c:v', 'hevc_nvenc',  '-y', f'{filedir}/{outfile}'
+      ]
     )
     for line in container.logs(stream=True, follow=True):
       text = line.decode("utf-8")
@@ -85,6 +96,13 @@ def run_convert(infile, outfile, stream_mapi, in_codec_name):
   except KeyboardInterrupt:
     api_client.stop(container.id)
     api_client.remove_container(container.id)
+  except Exception as e:
+    #barf stderr logs on fail
+    print(container.logs(stdout=True, stderr=True))
+    api_client.stop(container.id)
+    api_client.remove_container(container.id)
+    pprint(e)
+    sys.exit(1)
 
 
 def get_filepaths(filename):
@@ -102,21 +120,25 @@ def get_duration(filename):
   esc_infile = re.escape(filename)
   if not os.path.exists(filename):
     return False
-  container = client.containers.run(
-    image=FF_IMAGE,
-    remove=True,
-    devices=['/dev/dri:/dev/dri'],
-    environment=['PUID=1001', 'PGID=1001', 'AV_LOG_FORCE_NOCOLOR=1'],
-    volumes=[f'{filedir}:{filedir}'],
-    name=get_container_name('ffprobe_format'),
-    entrypoint='/usr/local/bin/ffprobe',
-    command=[ '-v', 'quiet', '-print_format', 'json', '-show_format', '-i', f'{filename}' ]
-    )
-  out = json.loads(container)
-  if 'format' in out:
-    if 'duration' in out['format']:
-      duration = out['format']['duration']
-      return float(duration)
+  try:
+    container = client.containers.run(
+      image=f'{FF_IMAGE}:{FF_TAG}',
+      remove=True,
+      devices=['/dev/dri:/dev/dri'],
+      environment=['PUID=1001', 'PGID=1001', 'AV_LOG_FORCE_NOCOLOR=1'],
+      volumes=[f'{filedir}:{filedir}'],
+      name=get_container_name('ffprobe_format', filename),
+      entrypoint='/usr/local/bin/ffprobe',
+      command=[ '-v', 'quiet', '-print_format', 'json', '-show_format', '-i', f'{filename}' ]
+      )
+    out = json.loads(container)
+    if 'format' in out:
+      if 'duration' in out['format']:
+        duration = out['format']['duration']
+        return float(duration)
+  except Exception as e:
+    pprint(e)
+    sys.exit(1)
   return False
 
 
@@ -127,12 +149,12 @@ def get_stream_map(filename):
   print(f"Getting stream maps: {filename}")
   try:
       container = client.containers.run(
-        image=FF_IMAGE,
+        image=f'{FF_IMAGE}:{FF_TAG}',
         remove=True,
         devices=['/dev/dri:/dev/dri'],
         environment=['PUID=1001', 'PGID=1001', 'AV_LOG_FORCE_NOCOLOR=1'],
         volumes=[f'{filedir}:{filedir}'],
-        name=get_container_name('ffprobe_streams'),
+        name=get_container_name('ffprobe_streams', filename),
         entrypoint='/usr/local/bin/ffprobe',
         command=[ '-v', 'quiet', '-print_format', 'json', '-show_streams', '-i', f'{filename}' ]
         )
@@ -144,7 +166,13 @@ def get_stream_map(filename):
   for num, stream in enumerate(streams):
     if stream['codec_type'] in ['audio', 'subtitle', 'video']:
       if stream['codec_type'] == 'video':
-        codec_name = stream['codec_name']
+        # only store the first video codec: todo this is lazy
+        if not codec_name:
+          # store any codec name we find
+          codec_name = stream['codec_name']
+          # oh, but if it's h264, then rewrite it to be nvidia encoder
+          if codec_name == 'h264':
+              codec_name = f"{codec_name}_cuvid"
       if stream['codec_type'] not in stream_map.values():
         if not stream.get('disposition', {'default': 0}).get('default', 0):
           stream_map[num] = stream['codec_type']
@@ -184,24 +212,17 @@ def get_outfile(infile):
   return outfile
 
 
-def get_pct(line, duration):
-  if line.startswith('frame=') and 'fps=' in line:
-    counted_frames = int(line.split('frame=')[-1].split('fps=')[0].strip())
-    if duration:
-        progress = counted_frames / 24
-        return float(progress) / duration
-  return 0
-
-
-GREEN_FG = '\x1b[38;5;46m'
-RED_FG   = '\x1b[38;5;196m'
-WHITE_FG = '\x1b[38;5;15m'
-BLUE_BG  = '\x1b[48;5;21m'
-GRAY_BG  = '\x1b[48;5;237m'
-RESET    = '\x1b[0m'
-
-
+################################################################################
 if __name__ == "__main__":
+
+  ## uncomment these sections to cleanup images and pull freshies
+  ## remove all old images TODO: only do this once a day
+  # images = [ x.id for x in client.images.list() if 'ffmpeg' in " ".join(x.tags) ]
+  # [ client.images.remove(image=x, force=True) for x in [ x.id for x in client.images.list() if 'ffmpeg' in " ".join(x.tags) ] ]
+
+  ## pull a fresh image
+  client.images.pull(FF_IMAGE, tag=FF_TAG)
+
   start = time.mktime(time.localtime())
   infile = ' '.join(sys.argv[1:])
   outfile = get_outfile(infile)
@@ -212,7 +233,9 @@ if __name__ == "__main__":
     if file_dur_check(infile, outfile):
       sys.exit(0)
   stream_map, in_codec_name  = get_stream_map(infile)
-  container = run_convert(infile, outfile, stream_map, in_codec_name)
-  file_dur_check(infile, outfile)
+  if run_convert(infile, outfile, stream_map, in_codec_name):
+      file_dur_check(infile, outfile)
+      os.chown(outfile, 1001, 1001)
   end = time.mktime(time.localtime())
   print(f"Done: Elapsed time: %s" % str(datetime.timedelta(seconds=end-start)))
+  client.containers.prune()
